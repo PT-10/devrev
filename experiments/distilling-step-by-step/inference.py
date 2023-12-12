0f52331a9722e8f24827fde20ee63059fe78c146
Sukriti Goyal <goyal.23@iitj.ac.in>
2023-12-12 21:34:30 +0530

    Adds README.md

--- experiments/distilling-step-by-step/inference.py
+++ experiments/distilling-step-by-step/inference.py
@@ -1,7 +1,7 @@
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 # Replace with your actual checkpoint folder path
-checkpoint_path = "ckpts/svamp/t5-v1_1-base/task_prefix/palm/1.0/llm/0.5/1024/8/AdamW/5e-05/0/checkpoint-10000"
+checkpoint_path = "/path/to/checkpoint/folder/"
 
 # Load the model and tokenizer using Transformers library
 model = T5ForConditionalGeneration.from_pretrained(checkpoint_path)
@@ -20,7 +20,8 @@ json_format = """[
                     }
                 ]"""
 
-input_text = f"Summarize all issues related to 'CUST123'. Give result as a json: {json_format}"
+input_text = f"YOUR INPUT TEXT"
+input_text += f" Give result as a json: {json_format}"
 
 # Tokenize input text
 input_ids = tokenizer.encode(input_text, return_tensors="pt")
